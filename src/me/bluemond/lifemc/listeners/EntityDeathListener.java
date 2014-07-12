68a00cabf6d618dccfc2e8e5fc284effd39ffad3
Staartvin <mijnleraar@msn.com>
2014-07-12 17:56:36 +0200

    Switch to PlayerSpawnListener and fix bed teleportation
    Signed-off-by: Staartvin <mijnleraar@msn.com>

--- src/me/bluemond/lifemc/listeners/EntityDeathListener.java
+++ src/me/bluemond/lifemc/listeners/EntityDeathListener.java
@@ -4,11 +4,10 @@ import me.bluemond.lifemc.LifeMC;
 import me.bluemond.lifemc.lang.Lang;
 
 import org.bukkit.Location;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 //Listener class
 public class EntityDeathListener implements Listener {
@@ -20,39 +19,54 @@ public class EntityDeathListener implements Listener {
 	}
 
 	@EventHandler
-	public void onEntityDeath(EntityDeathEvent event) {
+	public void onPlayerRespawn(PlayerRespawnEvent event) {
+		final Player player = event.getPlayer();
 
-		if (!event.getEntityType().equals(EntityType.PLAYER))
+		if (!player.hasPermission("lifemc.lives.lose"))
 			return;
 
-		Player player = (Player) event.getEntity();
-		
-		if (!player.hasPermission("lifemc.lives.lose")) return;
-		
 		int lives = plugin.getDataHandler().getLives(player);
-		
+
 		if (lives <= 1) {
-			
+
 			plugin.getDataHandler().setLives(player, 0);
-			
+
 			//player.getInventory().clear();
 			//player.getEquipment().clear();
 			//player.setExp(0);
-			
+
 			// Teleport player to bed so they spawn at their bed after they get unbanned.
 			if (plugin.getConfigHandler().spawnAtBedAfterBan()) {
-				Location bedLocation = player.getBedSpawnLocation();
+				final Location bedLocation = player.getBedSpawnLocation();
 
 				if (bedLocation != null) {
-					player.teleport(bedLocation);
+					System.out.print("Teleport to bed!");
+					System.out.print("Location to bed: " + bedLocation);
+
+					event.setRespawnLocation(bedLocation);
+					
+					// Teleport player after .25 seconds
+					plugin.getServer().getScheduler()
+							.runTaskLater(plugin, new Runnable() {
+								public void run() {
+									player.teleport(bedLocation);
+								}
+							}, 5L);
+					
 				}
 			}
-			
-			player.kickPlayer(Lang.KICK_OUT_OF_LIVES.getConfigValue());
+
+			// Kick player after .5 seconds
+			plugin.getServer().getScheduler()
+					.runTaskLater(plugin, new Runnable() {
+						public void run() {
+							player.kickPlayer(Lang.KICK_OUT_OF_LIVES.getConfigValue());
+						}
+					}, 10L);
 		} else {
-			
+
 			plugin.getDataHandler().setLives(player, lives - 1);
-			
+
 			player.sendMessage(Lang.LOST_A_LIFE.getConfigValue());
 		}
 	}
